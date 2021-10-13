842a356b945c7288c78a05a17ed8ae1f5e585733
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2021-10-14 00:41:48 +0530

    Add files via upload

---
+++ Practical-07/Practical-07_Task-7_v1.cpp
@@ -0,0 +1,25 @@
+/*
+	1   2   3              1   1   1
+	4   5   6      =>      2   2   2
+	7   8   9              3   3   3      
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int N = 3;
+	int arr[N][N];
+	for(int i = 0 ; i < N ; i++){
+		for(int j = 0 ; j < N ; j++)
+			cin >> arr[i][j];
+	}for(int i = 0 ; i < N ; i++){
+		for(int j = 0 ; j < N ; j++)
+			arr[i][j] = i+1;
+	}cout << "\nResultant array : " << endl;
+	for(int i = 0 ; i < N ; i++){
+		for(int j = 0 ; j < N ; j++)
+			cout << arr[i][j] << " ";
+		cout << "\n";
+	}return 0;
+}
