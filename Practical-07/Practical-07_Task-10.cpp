ac8f3799d00bb460c2ce7d01a00b7e0a240a4af7
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2021-10-14 10:10:55 +0530

    Add files via upload

---
+++ Practical-07/Practical-07_Task-10.cpp
@@ -0,0 +1,34 @@
+/*
+                                                                              Print the following pattern
+                                                                                                   *
+                                                                                                 * *
+                                                                                               * * *
+                                                                                             * * * *
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+     int N;
+     cout << "Enter the number of rows : " << endl;
+     cin >> N;
+     char arr[N][N];
+     for(int i = 0 ; i < N ; i++){
+          for(int j = 0 ; j < N ; j++){
+               if((i + j) >= (N-1))
+                    arr[i][j] = '*';
+               else
+                    arr[i][j] = ' ';
+          }
+     }
+     //Printing the array
+     for(int i = 0 ; i < N ; i++){
+          for(int j = 0 ; j < N ; j++)
+               cout << arr[i][j] << " ";
+          cout << "\n";
+      }
+     
+     
+     return 0;
+}     
