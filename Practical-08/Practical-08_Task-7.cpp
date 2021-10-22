fd4f09269bce5a176300e45b860372d2c7603902
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2021-10-22 17:05:53 +0530

    Add files via upload

---
+++ Practical-08/Practical-08_Task-7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+int main()
+{
+  int num;
+  cout << "Enter the number : "<<endl;
+  cin >> num;
+  cout << printPattern(num) << endl;
+  return 0;
+}
