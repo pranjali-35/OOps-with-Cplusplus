5ef845527169c6c45d4841fde5597b981978c814
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2021-12-27 18:23:24 +0530

    Create Practical-11_Task1_b.cpp

---
+++ Practical-11/Practical-11_Task1_b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void displayA()
+    {
+        cout << " Class A Method Called " << endl;
+    }
+};
+class B : public A
+{
+
+public:
+    void displayB()
+    {
+        cout << "calling Class A method from class B method" << endl;
+        displayA();
+    }
+};
+int main()
+{
+    B obj;
+    obj.displayA();
+}
